b509c4d50b7831bc45d676d2655940ff77d89aeb
Ijat-game <ijat.game@gmail.com>
2015-01-21 20:33:10 -0600

    Filled in the Comments for the assignment.  1/3 of grade.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program will create a matrix and then allow for the 
+## inversion of the matrix.  If the matrix has already been inverted 
+## it pulls it from cache instead of redoing the work.  If not 
+## inverted then it inverts the matrix and also saves a copy to a cache.
 
-## Write a short comment describing this function
+
+## Create a Matrix from passed matrix same size and shape.   
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of the passed matrix.  Cache inverse matrix allowing for
+## checking if the matrix has already been inverted. if not inverted then 
+## invert save and return. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
